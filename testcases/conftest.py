f1326241d359abd9b085be7a6159393288ea319f
somapujari <somapujari0@gmail.com>
2024-09-07 18:49:27 +0530

    chromeoptions changed

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 import pytest
+from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture
 def setup():
-    driver = webdriver.Chrome()
+    chromeoptions = webdriver.ChromeOptions()
+    chromeoptions.binary_location = r'C:\Users\Dell\AppData\Local\Google\Chrome\Application\chrome.exe'
+    path = r'C:\Users\Dell\AppData\Local\Programs\Python\Python310\chromedriver.exe'
+    service = Service(path)
+    service.start()
+    driver = webdriver.Chrome(service=service, options=chromeoptions)
     return driver
